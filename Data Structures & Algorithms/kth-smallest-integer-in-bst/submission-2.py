690e7ef5851390d18db50e2d5367c400bdbca9fe
Phoebe Chen <phoebechen.rwd@gmail.com>
2026-07-28 12:30:00 -0700

    Add: kth-smallest-integer-in-bst - submission-2

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-2.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        
+        cnt = k
+        res = root.val
+
+        def inorder(node):
+            nonlocal cnt, res # 在局部函数里修改不可变的全局变量的时候要用nonlocal声明，如果只读不需要nonlocal声明
+
+            if not node:
+                return None
+
+            inorder(node.left)
+
+            # if cnt == 0: # 如果上面递归左边之后cnt已经为0，则直接返回
+            #    return
+
+            cnt -= 1 # 每经过一个节点，计数器就-1
+            if cnt == 0: # 当计数器降到0时，当前节点就是我们要找的答案
+                res = node.val
+                return
+
+            inorder(node.right)
+
+        inorder(root)
+        return res
